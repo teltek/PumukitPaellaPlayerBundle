8dc4ad76bbc301fd444cf00bf12430272636291d
Alfro <arodriguez@teltek.es>
2016-06-02 17:13:44 +0200

    #10825 Controller: Added playlist API for paella.

--- Controller/PaellaRepositoryController.php
+++ Controller/PaellaRepositoryController.php
@@ -3,6 +3,7 @@
 namespace Pumukit\PaellaPlayerBundle\Controller;
 
 use Pumukit\SchemaBundle\Document\MultimediaObject;
+use Pumukit\SchemaBundle\Document\Series;
 use Pumukit\SchemaBundle\Document\Track;
 use Pumukit\VideoEditorBundle\Document\Annotation;
 use Pumukit\WebTVBundle\Controller\WebTVController;
@@ -30,4 +31,17 @@ class PaellaRepositoryController extends Controller implements WebTVController
         return new Response($response);
     }
 
+    /**
+     * @Route("/paellaplaylist/{id}.{_format}", defaults={"_format"="json", "no_channels":true}, requirements={"_format": "json|xml"})
+     * @Route("/secret/paellaplaylist/{secret}.{_format}", defaults={"_format":"json", "show_hide":true, "no_channels":true}, requirements={"_format": "json|xml"})
+     * @Method("GET")
+     */
+    public function playlistAction(Series $series, Request $request)
+    {
+        $serializer = $this->get('serializer');
+        $paellaDataService = $this->get('pumukitpaellaplayer.paelladata');
+        $data = $paellaDataService->getPaellaPlaylistData($series, $request);
+        $response = $serializer->serialize($data, $request->getRequestFormat());
+        return new Response($response);
+    }
 }
