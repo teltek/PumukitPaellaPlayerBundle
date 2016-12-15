1fe6a28a5538f6206be289e0732a21f979066682
rubenrua <rubenrua@teltek.es>
2016-12-15 11:34:08 +0100

    #12718 Disable autoplay on iframes

--- Controller/BasePlayerController.php
+++ Controller/BasePlayerController.php
@@ -67,7 +67,7 @@ class BasePlayerController extends BasePlayerControllero
         //Then just return several tracks.
         $tracks = array($track);
 
-        return array('autostart' => $request->query->get('autostart', 'true'),
+        return array('autostart' => $request->query->get('autostart', 'false'),
                      'intro' => $this->getIntro($request->query->get('intro')),
                      'multimediaObject' => $multimediaObject,
                      'object' => $multimediaObject,
