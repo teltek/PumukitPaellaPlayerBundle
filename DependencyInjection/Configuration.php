5b47b44fd1327601796adba300c4ba666eb5b227
rubenrua <rubenrua@teltek.es>
2017-06-15 17:01:04 +0200

    TTK-14076 Add a custom css url II

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -13,7 +13,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('pumukit_paella');
+        $rootNode = $treeBuilder->root('pumukit_paella_player');
 
         $rootNode
           ->children()
