01e0175671994ee3e4c20ad3339fb1a3a99a46f9
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2022-10-06 12:02:25 +0200

    Update Paella plugins and features

--- Resources/public/paella/src/index.js
+++ Resources/public/paella/src/index.js
@@ -1,4 +1,4 @@
-import { Paella,defaultLoadVideoManifestFunction, utils } from 'paella-core';
+import {Paella,defaultLoadVideoManifestFunction, utils, Events} from 'paella-core';
 import getBasicPluginContext from 'paella-basic-plugins';
 import getSlidePluginContext from 'paella-slide-plugins';
 import getZoomPluginContext from 'paella-zoom-plugin';
@@ -18,9 +18,32 @@ window.onload = async () => {
         var configID = location.pathname.split('/').slice(-1)[0];
     }
 
+    let hasIntro = false;
+    let hasTail = false;
     let introLoaded = false;
+    let tailLoaded = false;
+    let originalVideoLoaded = false;
 
     const initParams = {
+        loadVideoManifest: async (videoManifestUrl, config, player) => {
+            const result = await defaultLoadVideoManifestFunction(videoManifestUrl, config, player);
+
+            hasIntro = (typeof result.intro !== 'undefined');
+            hasTail = (typeof result.tail !== 'undefined');
+
+            if (hasIntro && !introLoaded) {
+                videoManifestUrl = result.intro;
+                return await defaultLoadVideoManifestFunction(videoManifestUrl, config, player);
+            }
+
+            if(originalVideoLoaded && hasTail && !tailLoaded) {
+                videoManifestUrl = result.tail;
+                tailLoaded = true;
+                return await defaultLoadVideoManifestFunction(videoManifestUrl, config, player);
+            }
+
+            return result;
+        },
         customPluginContext: [
             getBasicPluginContext(),
             getSlidePluginContext(),
@@ -28,25 +51,12 @@ window.onload = async () => {
             getUserTrackingPluginContext(),
             getTeltekPluginsContext()
         ],
-
-        // Aquí puedes personalizar las URLs de carga del fichero de configuración, en el caso de que no lo obtengas
-        // de la ubicación por defecto
         configResourcesUrl: '/paella/',
         configUrl: '/paella/config.json' + '?configID='+ configID,
-
-        // Aquí puedes personalizar la URL de obtención de los datos del vídeo, en concreto, la parte estática de la URL
         repositoryUrl: '/paellarepository/',
-
-        // Esta función sirve para que devuelvas el identificador único del vídeo con el cual puedes obtener sus datos
-        // desde tu portal.
         getVideoId: (config, player) => {
-            // En la implementación por defecto, se obtiene del parámetro `id` de la URL, en tu caso puedes personalizar
-            // este parámetro para obtener el vídeo desde otro parámetro, desde una cookie o como consideres oportuno
             return location.pathname.split('/').slice(-1)[0];
         },
-        // Esta parte es para formar la URL complete de la carga del video. Recibe como parámetro la URL anterior
-        // (respositoryUrl), así que en la práctica podrías implementar aquí la URL completa y prescindir de
-        // la función anterior. El parámetro `videoId` se obtiene de la función `getVideoId`
         getManifestUrl: (repoUrl,videoId) => {
             let location = window.location.href;
             const params = new URLSearchParams(window.location.search);
@@ -63,47 +73,15 @@ window.onload = async () => {
                 return '/paellaplaylist/' + playlistId + '?videoPos=' + pos;
             }
 
-            if(location.search("secret") != -1) {
+            if(location.search("secret") !== -1) {
                 return '/secret' + `${repoUrl}${videoId}`;
             }
 
             return `${repoUrl}${videoId}`;
         },
-
-        // Esta función devuelve la URL complete del archivo manifest del vídeo. En la implementación por defecto de
-        // paella player, aquí se añade el nombre de archivo /data.json, pero en Opencast no es necesario modificar
-        // la url, así que la devolvemos tal cual
         getManifestFileUrl: (manifestUrl) => {
             return manifestUrl;
         },
-
-        // Esta es la función donde se realiza la conversión del fichero manifest de opencast al formato de paella player.
-        // Seguramente la mayor parte de la implementación la tendrás que hacer aquí. Ten en cuenta que es una función asíncrona,
-        // así que tendrás que implementarla asíncrona o bien utilizar promesas. Por lo que me cuentas, el problema que tienes
-        // seguramente estará aquí.
-
-        loadVideoManifest: async function (url, config, player) {
-            // Aquí es donde tendrías que hacer la llamada a tu portal para obtener los datos
-            // del vídeo para formar el manifest en el formato de paella, tal cual indica en la
-            // documentación:
-            // https://github.com/polimediaupv/paella-core/blob/main/doc/video_manifest.md
-            // Esto es un código de ejemplo, aquí tendrías que modificar lo que haga falta para adaptarlo a tu portal
-
-            // Obtener los streams de la intro, vienen dados en el manifest.
-            const result = await loadVideoManifest(videoManifestUrl, config, player);
-            if (introLoaded) {
-                return result;
-            }
-            else {
-                result.streams = result.intro;
-            }
-
-            const response = await fetch(url);
-            const pumukitVideoData = await response.json();
-
-            return pumukitVideoData;
-
-        }
     };
 
     class PaellaPlayer extends Paella {
@@ -127,4 +105,19 @@ window.onload = async () => {
     } catch (e) {
         console.error(e);
     }
+
+    paella.bindEvent(Events.ENDED, async () => {
+        if (hasIntro && !introLoaded) {
+            introLoaded = true;
+            await paella.reload();
+        }
+
+        if(originalVideoLoaded && hasTail && !tailLoaded) {
+            await paella.reload();
+        }
+
+        originalVideoLoaded = true;
+
+    }, false);
+
 }
