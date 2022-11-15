58444beb93edac19864414e59af4e9ba4de4f846
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2022-11-15 13:52:18 +0100

    Remove annotationsManifest

--- Services/VoDManifest.php
+++ Services/VoDManifest.php
@@ -13,29 +13,25 @@ class VoDManifest
     protected $frameListManifest;
     protected $captionsManifest;
     protected $customManifest;
-    protected $annotationsManifest;
 
     public function __construct(
         MetadataManifest $metadataManifest,
         StreamsManifest $streamsManifest,
         FrameListManifest $frameListManifest,
         CaptionsManifest $captionsManifest,
-        CustomManifest $customManifest,
-        AnnotationsManifest $annotationsManifest
+        CustomManifest $customManifest
     ) {
         $this->metadataManifest = $metadataManifest;
         $this->streamsManifest = $streamsManifest;
         $this->frameListManifest = $frameListManifest;
         $this->captionsManifest = $captionsManifest;
         $this->customManifest = $customManifest;
-        $this->annotationsManifest = $annotationsManifest;
     }
 
     public function create(MultimediaObject $multimediaObject, ?string $trackId): array
     {
         $data['metadata'] = $this->metadataManifest->create($multimediaObject);
         $data = $this->customManifest->completeManifestData($multimediaObject, $data);
-        $data['breaks'] = $this->annotationsManifest->create($multimediaObject);
         $data['streams'] = $this->streamsManifest->createStreamsForVoD($multimediaObject, $trackId)['streams'];
         $data['frameList'] = $this->frameListManifest->create($multimediaObject);
         $data['captions'] = $this->captionsManifest->create($multimediaObject);
