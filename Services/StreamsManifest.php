4f901926b2980a3792e06c26a2fb79bf06cae9a2
Alba <albalago.18@gmail.com>
2024-12-09 09:25:54 +0100

    TTK-27392: Add metadata to access codec

--- Services/StreamsManifest.php
+++ Services/StreamsManifest.php
@@ -171,7 +171,7 @@ class StreamsManifest
             }
         }
 
-        if ($sbsTrack && in_array($sbsTrack->codecName(), $availableCodecs)) {
+        if ($sbsTrack && in_array($sbsTrack->metadata()->codecName(), $availableCodecs)) {
             $tracks['sbs'][] = $sbsTrack;
         }
 
