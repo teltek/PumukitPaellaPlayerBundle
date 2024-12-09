228e4143e022f9dc570f7b9cdf9f91bddbd8ba5b
albacodina <48245111+albacodina@users.noreply.github.com>
2024-12-09 09:27:15 +0100

    Bugfix 27392 change codcename access (#166)
    * TTK-27392: Change codecname access
    
    * TTK-27392: Add metadata to access codec

--- Services/StreamsManifest.php
+++ Services/StreamsManifest.php
@@ -171,7 +171,7 @@ class StreamsManifest
             }
         }
 
-        if ($sbsTrack && in_array($sbsTrack->getVcodec(), $availableCodecs)) {
+        if ($sbsTrack && in_array($sbsTrack->metadata()->codecName(), $availableCodecs)) {
             $tracks['sbs'][] = $sbsTrack;
         }
 
